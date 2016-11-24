dbe98a72533bb2baf5b5c57f5d3c1915e06ab277
adrianaVN <venegasadriana78@gmail.com>
2016-11-24 22:19:37 +0000

    ya no se que le movi

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function makeArray(n) {
+/*function makeArray(n) {
 	this.length = n;
 	for (var i=1; i<=n; i++) {
 		this[i] = ""; 
@@ -113,3 +113,139 @@ function nombre(){
 	    }
 
 }
+*/
+//DATOS MEXICO
+var imgMex;
+imgMex = [];
+imgMex[0] = {
+	nombre : "mariana",
+    foto: "images/mexico/mariana.jpg"
+	
+};
+imgMex[1] = {
+	nombre: "karem",
+	foto: "images/mexico/karem.jpg"
+};
+imgMex[2] = {
+	nombre : "varinia",
+	foto : "images/mexico/varinia.jpg"
+};
+var imgLim;
+imgLim = [];
+imgLim[0] = {
+	nombre : "mariana",
+    foto: "images/mexico/mariana.jpg"
+	
+};
+imgLim[1] = {
+	nombre: "karem",
+	foto: "images/mexico/karem.jpg"
+};
+imgLim[2] = {
+	nombre : "varinia",
+	foto : "images/mexico/varinia.jpg"
+};
+var imgAre;
+imgAre = [];
+imgAre[0] = {
+	nombre : "mariana",
+    foto: "images/mexico/mariana.jpg"
+	
+};
+imgAre[1] = {
+	nombre: "karem",
+	foto: "images/mexico/karem.jpg"
+};
+imgAre[2] = {
+	nombre : "varinia",
+	foto : "images/mexico/varinia.jpg"
+};
+var imgSan;
+imgSan = [];
+imgSan[0] = {
+	nombre : "mariana",
+    foto: "images/mexico/mariana.jpg"
+	
+};
+imgSan[1] = {
+	nombre: "karem",
+	foto: "images/mexico/karem.jpg"
+};
+imgSan[2] = {
+	nombre : "varinia",
+	foto : "images/mexico/varinia.jpg"
+};
+// ARREGLO GENERAL
+var imgGeneral;
+var imgGeneral = [];
+imgGeneral[0] = imgMex;
+imgGeneral[1] = imgLim;
+imgGeneral[2] = imgAre;
+imgGeneral[3] = imgSan;
+console.log(imgGeneral);
+
+//ELEMENTOS
+var inputPais = document.getElementById("pais");
+var btnComprobar = document.getElementById("comprobar");
+var colocarImg = document.getElementById("foto");
+var puntaje = document.getElementById("puntaje");
+var mensaje = document.getElementById("mensaje");
+var intentos = 0;
+var pts = 5;
+
+inputPais.onchange = function() {
+	var numPais = inputPais.value;
+	var pais = imgGeneral[numPais];
+	console.log("numero pais " + numPais + " ---- pais: " + pais )
+	
+	//imagen aleatoria
+	var i;
+	function elegirImagen(min, max) {
+		i = Math.floor(Math.random() * pais.length);
+		colocarImg.innerHTML= ("<img src=" + pais[i].foto + " style = 'width:100%'>");
+		colocarImg.className = "img"
+
+		return i;
+
+	}
+	elegirImagen();
+	//colocar imagen correspondiente
+	
+
+//COMPROBAR INPUT CON NOMBRE
+btnComprobar.onclick = function () {
+	var adivinar = document.getElementById("adivinar").value; //input nombre
+	adivinar = adivinar.toLowerCase();
+	adivinar = adivinar.trim();
+		if (adivinar == pais[i].nombre) {
+			puntaje.innerHTML = (pts);
+			console.log("es correcto");
+			mensaje.innerHTML = ("Exelente acertaste!");
+			mensaje.className = "bg-verde text-center mt-5";	
+			pts = pts;	
+			puntaje.className = "bg-verde text-center";
+			console.log(pais.length);
+			pais.splice(i,1);
+			console.log(pais.length);
+			elegirImagen();
+		}else if (intentos === 5){
+				//borrar value del input
+				pts = pts - 1;
+				puntaje.className = "bg-rojo text-center";
+				puntaje.innerHTML = (pts);	
+				elegirImagen();
+				intentos = intentos;
+			}
+
+		 else {
+			mensaje.innerHTML = ("sigue intentandolo");
+			mensaje.className = "bg-rojo text-center mt-5";
+			intentos = intentos;
+			
+				/*alert("intentalo de nuevo");*/		
+			
+		}
+		document.getElementById("adivinar").value = "";
+
+	}
+};
